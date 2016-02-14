d0fe6e292c0e797abbc491e6c051e8cf3d40ee19
guillochon <guillochon@gmail.com>
2016-02-13 16:17:33 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>astrocats.space</title>
+<link href="astrocats.css" rel="stylesheet" media="all"/>
+</head>
+<body>
+<div class="title">Open Astronomy<br>&amp; Astrophysics Catalogs</div>
+<table class="cat-tab">
+<tbody>
+<tr><td><img class="circle" src="cat1.jpg"></td><td><a href="https://sne.space" class="hvr-fade">Open Supernova Catalog</a></td></tr>
+<tr><td><img class="circle" src="cat2.jpg"></td><td><a href="https://tde.space" class="hvr-fade">Open TDE Catalog</a></td></tr>
+<tr><td><img class="circle" src="cat3.jpg"></td><td><a href="https://galcenter.space" class="hvr-fade">Open Galactic Center Catalog</a></td></tr>
+<tr><td><img class="circle" src="cat4.jpg"></td><td><a href="https://holes.space" class="hvr-fade">Open Black Hole Catalog</a></td></tr>
+</tbody>
+</table>
+</body>
+</html>
