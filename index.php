0bb949cf38d6d559f840a13c78eb203472b9c6df
guillochon <guillochon@gmail.com>
2016-02-13 19:54:48 -0800

    Now looks nice.

--- index.php
+++ index.php
@@ -2,16 +2,35 @@
 <head>
 <title>astrocats.space</title>
 <link href="astrocats.css" rel="stylesheet" media="all"/>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-6237166-7', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
 <body>
-<div class="title">Open Astronomy<br>&amp; Astrophysics Catalogs</div>
+<div class="main">
+<div class="title">astrocats.space</div>
+<div class="subtitle">Open Astronomy Catalogs</div>
 <table class="cat-tab">
 <tbody>
-<tr><td><img class="circle" src="cat1.jpg"></td><td><a href="https://sne.space" class="hvr-fade">Open Supernova Catalog</a></td></tr>
-<tr><td><img class="circle" src="cat2.jpg"></td><td><a href="https://tde.space" class="hvr-fade">Open TDE Catalog</a></td></tr>
-<tr><td><img class="circle" src="cat3.jpg"></td><td><a href="https://galcenter.space" class="hvr-fade">Open Galactic Center Catalog</a></td></tr>
-<tr><td><img class="circle" src="cat4.jpg"></td><td><a href="https://holes.space" class="hvr-fade">Open Black Hole Catalog</a></td></tr>
+<tr><td><a target="_blank" href="https://sne.space" class="cat1 circle"></a></td><td><a target="_blank" href="https://sne.space" class="hvr-fade">Open Supernova Catalog</a></td><td class="rightdiv"></td></tr>
+<tr><td><a target="_blank" href="https://tde.space" class="cat2 circle"></td><td><a target="_blank" href="https://tde.space" class="hvr-fade">Open TDE Catalog</a></td><td class="rightdiv"></td></tr>
+<tr><td><a target="_blank" href="http://www.openexoplanetcatalogue.com/" class="cat5 circle"></td><td><a target="_blank" href="http://www.openexoplanetcatalogue.com/" class="hvr-fade">Open Exoplanet Catalogue</a></td><td class="rightdiv"></td></tr>
 </tbody>
 </table>
+<div class="note">The following catalogs are in beta</div>
+<table class="cat-tab">
+<tbody>
+<tr><td><a target="_blank" href="https://galcenter.space" class="cat3 circle"></td><td><a target="_blank" href="https://galcenter.space" class="hvr-fade">Open Galactic Center Catalog</a></td><td class="rightdiv"></td></tr>
+<tr><td><a target="_blank" href="https://holes.space" class="cat4 circle"></td><td><a target="_blank" href="https://holes.space" class="hvr-fade">Open Black Hole Catalog</a></td><td class="rightdiv"></td></tr>
+</tbody>
+</table>
+</div>
 </body>
 </html>
