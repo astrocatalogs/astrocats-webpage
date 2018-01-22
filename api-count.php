fb57083c841f0c8ac25148384e109995c3c76473
guillochon <guillochon@gmail.com>
2018-01-22 09:50:48 -0800

    ENH: count now includes rolling logs.

--- api-count.php
+++ api-count.php
@@ -2,30 +2,38 @@
 function MakeReadable($num) {
 	if ($num == 0) return '0';
 		$i = floor(log($num, 1000));
-		return round($num / pow(1000, $i), [0,0,2,2,3][$i]).['','k','M','G','T'][$i];
+		return round($num / pow(1000, $i), [1,1,2,2,3][$i]).['','k','M','G','T'][$i];
 }
 
 header('Content-Type: application/json');
-$file="/var/log/oacapi.log";
+$patt="/var/log/oacapi.log*";
 
-if (file_exists($file)) {
-	$linecount = 0;
-	$handle = fopen($file, "r");
-	while(!feof($handle)){
-		$line = fgets($handle);
-		preg_match('/([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})/', $line, $match);
-		preg_match('/(Query)/', $line, $qmatch);
-		if (count($qmatch) > 0) {
-			$time = strtotime($match[0]);
-			if ((time() - $time) <= 86400) {
-				$linecount++;
+$days = 7;
+
+$linecount = 0;
+foreach (glob($patt) as $file) {
+	$date = explode('-', $file);
+	if (count($date) == 2) {
+		$date = $date[1];
+		$sdate = substr($date, 0, 4) . '-' . substr($date, 4, 2) . '-' . substr($date, -2);
+		if (time() - strtotime($sdate) > $days*86400) continue;
+	}
+	if (file_exists($file)) {
+		$handle = fopen($file, "r");
+		while(!feof($handle)){
+			$line = fgets($handle);
+			preg_match('/([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})/', $line, $match);
+			preg_match('/(Query successful)/', $line, $qmatch);
+			if (count($qmatch) > 0) {
+				$time = strtotime($match[0]);
+				if ((time() - $time) <= $days*86400) {
+					$linecount++;
+				}
 			}
 		}
+		
+		fclose($handle);
 	}
-	
-	fclose($handle);
-} else {
-	$linecount = 0;
 }
 
 $arr = array('count' => MakeReadable($linecount));
