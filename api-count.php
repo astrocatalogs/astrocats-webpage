5b354563e379c2c9734eb4b1972b672bdaab2b43
guillochon <guillochon@gmail.com>
2018-03-02 09:02:33 -0800

    Add trending/top5 to output

--- api-count.php
+++ api-count.php
@@ -19,6 +19,7 @@ $patt="/var/log/oacapi.log*";
 $days = 7;
 $day_secs = $days * 86400;
 $ips = array();
+$events = array();
 
 $linecount = 0;
 foreach (glob($patt) as $file) {
@@ -30,29 +31,59 @@ foreach (glob($patt) as $file) {
 	}
 	if (file_exists($file)) {
 		$handle = fopen($file, "r");
+		$lc = 0;
 		while(!feof($handle)){
 			$line = fgets($handle);
 			preg_match('/(Query successful)/', $line, $qmatch);
-			preg_match('/(Query from )(.+): (.+) --/', $line, $imatch);
-			if (array_key_exists($imatch[2], $ips)) {
-				$ips[$imatch[2]]++;
-			} else {
-				$ips[$imatch[2]] = 0;
-			}
 			if (count($qmatch) > 0) {
 				preg_match('/([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})/', $line, $match);
 				$time = strtotime($match[0]);
 				if ((time() - $time) <= $day_secs) {
 					$linecount++;
 				}
+				continue;
 			}
+			preg_match('/(Query unsuccessful)/', $line, $umatch);
+			if (count($umatch) > 0) {
+				if ($lc <= $lastlc + 3) {
+					if (array_key_exists($lastevent, $events)) {
+						if ($events[$lastevent] == 1) {
+							unset($events[$lastevent]);
+						} else {
+							$events[$lastevent]--;
+						}
+					}
+				}
+				continue;
+			}
+			preg_match('/(Query from )(.+): (.+) -- (.+)/', $line, $imatch);
+			if (count($imatch) == 0) continue;
+
+			if (array_key_exists($imatch[2], $ips)) {
+				$ips[$imatch[2]]++;
+			} else {
+				$ips[$imatch[2]] = 1;
+			}
+			$event = explode('/', $imatch[4])[0];
+			if (!in_array($event, array('None', 'all', 'catalog'))) {
+				if (array_key_exists($event, $events)) {
+					$events[$event]++;
+				} else {
+					$events[$event] = 1;
+				}
+				$lastlc = $lc;
+				$lastevent = $event;
+			}
+			$lc++;
 		}
 		
 		fclose($handle);
 	}
 }
 
-$arr = array('count' => MakeReadable($linecount), 'unique' => MakeReadable(count($ips)));
+arsort($events);
+$arr = array('count' => MakeReadable($linecount), 'unique' => MakeReadable(count($ips)),
+	'trending' => $events, 'top5' => implode(", ", array_slice(array_keys($events), 0, 5)));
 $output = json_encode($arr);
 file_put_contents($cache_path, $output);
 echo $output;
